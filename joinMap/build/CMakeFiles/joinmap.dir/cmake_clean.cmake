c961525e0d2eaa8589195bf043685877bc54656c
dongfangliu <liuwj@shanghaitech.edu.cn>
2017-10-19 20:24:28 +0800

    joinMap，RGBD点云拼接

---
+++ joinMap/build/CMakeFiles/joinmap.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/joinmap.dir/main.o"
+  "joinmap.pdb"
+  "joinmap"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/joinmap.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
